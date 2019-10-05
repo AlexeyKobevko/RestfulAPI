78c07db5cf8dadae3a08fe835a3257cd6de729f9
Alexey Kobevko <folco_2000@mail.ru>
2019-10-05 19:47:17 +0300

    My first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+}).then(() => {
+  console.log("Successfully connected to the database");
+}).catch(err => {
+  console.log("Could not connect to the database. Exiting now...", err);
+  process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+  res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// Require Notes routes
+require('./app/routes/note.routes')(app);
+
+// listen for requests
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
